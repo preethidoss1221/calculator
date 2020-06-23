205687bd74bf3f92bf9c9fe3d69a9da08d141647
preethidoss1221 <67315206+preethidoss1221@users.noreply.github.com>
2020-06-23 20:10:04 +0530

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   //additional changes
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
